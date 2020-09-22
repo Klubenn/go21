66e8a32530423aab8d6178c1ecb80dc12b883491
Klubenn <snape@mail.ru>
2020-09-22 17:23:46 +0300

    almost finished D01, ex01

--- Day01/compareDB/compareDB.go
+++ Day01/compareDB/compareDB.go
@@ -3,22 +3,89 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"strings"
 )
 
-func main() {
-	var old, new bool
+func checkInput() (string, string) {
+	var old, new1 bool
 	oldBase := flag.String("old", "", "old database file")
 	newBase := flag.String("new", "", "new database file")
 	flag.Parse()
 	if *oldBase == "" || *newBase == "" {
 		log.Fatalln("Both: old and new database files should be specified")
 	}
-	if old = strings.HasSuffix(*oldBase, ".json"); old {
-		if new = strings.HasSuffix(*newBase, ".xml"); !new {
-			log.Fatalln("Old and ")
+	if old = strings.HasSuffix(*oldBase, ".json"); !old {
+		old = strings.HasSuffix(*oldBase, ".xml")
+	}
+	if new1 = strings.HasSuffix(*newBase, ".json"); !new1 {
+		new1 = strings.HasSuffix(*newBase, ".xml")
+	}
+	if !old || !new1 {
+		log.Fatalln("Each database should have either '.json' or '.xml' extension")
+	}
+	return *oldBase, *newBase
+}
+
+func getInfoFromDB(file string) Recipes {
+	var db DBReader
+	var js *JSON
+	var xm *XML
+
+	dat, err := ioutil.ReadFile(file)
+	if err != nil {
+		log.Fatalln("Error: can't read from file:", err)
+	}
+
+	if strings.HasSuffix(file, ".json") {
+		db = js
+	} else {
+		db = xm
+	}
+	result, err := db.recipy(dat)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return result
+}
+
+func compareRecipy(oldCake, newCake Cake) {
+	if oldCake.Time != newCake.Time {
+		fmt.Println("CHANGED cooking time for cake \"%s\" - \"%s\" instead of \"%s\"", oldCake.Name, newCake.Time, oldCake.Time)
+	}
+	
+}
+
+func compareDB(oldDB, newDB Recipes) {
+	for _, oldCake := range oldDB.Cake {
+		var cakePresent bool
+		for _, newCake := range newDB.Cake {
+			if oldCake.Name == newCake.Name {
+				cakePresent = true
+				compareRecipy(oldCake, newCake)
+			}
+		}
+		if !cakePresent {
+			fmt.Println("REMOVED cake \"%s\"", oldCake.Name)
 		}
 	}
-	fmt.Println(*oldBase, *newBase)
+	for _, newCake := range newDB.Cake {
+		var cakePresent bool
+		for _, oldCake := range oldDB.Cake {
+			if newCake.Name == oldCake.Name {
+				cakePresent = true
+			}
+		}
+		if !cakePresent {
+			fmt.Println("ADDED cake \"%s\"", newCake.Name)
+		}
+	}
+}
+
+func main() {
+	oldFile, newFile := checkInput()
+	oldDB := getInfoFromDB(oldFile)
+	newDB := getInfoFromDB(newFile)
+	compareDB(oldDB, newDB)
 }
--- Day01/compareDB/parsing.go
+++ Day01/compareDB/parsing.go
@@ -1,3 +1,45 @@
 package main
 
+import (
+	"encoding/json"
+	"encoding/xml"
+)
+
+type Cake struct {
+	Name        string `json:"name" xml:"name"`
+	Time        string `json:"time" xml:"stovetime"`
+	Ingredients []struct {
+		IngredientName  string `json:"ingredient_name" xml:"itemname"`
+		IngredientCount string `json:"ingredient_count" xml:"itemcount"`
+		IngredientUnit  string `json:"ingredient_unit,omitempty" xml:"itemunit,omitempty"`
+	} `json:"ingredients" xml:"ingredients>item"`
+}
+
+type Recipes struct {
+	Cake [] Cake `json:"cake" xml:"cake"`
+}
+
+type JSON struct {
+}
+
+type XML struct {
+}
+
+func (r *JSON) recipy(dat []byte) (Recipes, error) {
+	var data Recipes
+	err := json.Unmarshal(dat, &data)
+
+	return data, err
+}
+
+func (r *XML) recipy(dat []byte) (Recipes, error) {
+	var data Recipes
+	err := xml.Unmarshal(dat, &data)
+
+	return data, err
+}
+
+type DBReader interface {
+	recipy(dat []byte) (Recipes, error)
+}
 
